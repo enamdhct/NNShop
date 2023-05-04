d287fb0fc05d60b0f9acafbc813e861e4b9821f0
enamdhct <tranwuocnam@gmail.com>
2023-05-05 01:06:28 +0700

    first commit

---
+++ NNShop/FE/FE_guest/src/components/Slideshow.vue
@@ -0,0 +1,46 @@
+<style>
+    .slideshow {
+        border-radius: 10px;
+    }
+    .slideshow img {
+        height: 650px;
+        width: 100%;
+    }
+</style>
+<template>
+        <div id="carouselExampleIndicators" class="carousel slide slideshow shadow-sm" data-ride="carousel" >
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="https://media.bongdasky.com/thumb_w/editors/2021/07/26/lyLabDCR3rbEHjLIrWMWEG8iz55mNW8trlPads3N_680.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="https://media.bongda.com.vn/files/huy.phandang/2022/07/26/4194-0700.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/if_w_gt_1920,w_1920/enGB/Images/football-ss23-italy-feds-away-pdp-mh-d_tcm143-974355.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="https://media.bongda.com.vn/files/huy.phandang/2022/07/26/4201-0700.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="https://brand.assets.adidas.com/image/upload/f_auto,q_auto,fl_lossy/if_w_gt_1920,w_1920/enGB/Images/football-fw22-manchesterunited-away-pdp-masthead-d_tcm143-907213.jpg" class="d-block w-100" alt="...">
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-target="#carouselExampleIndicators" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-target="#carouselExampleIndicators" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </button>
+    </div>
+    
+</template>
